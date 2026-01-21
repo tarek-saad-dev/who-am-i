a2197838bc474b28df24ce2a3b7f871aca2089d9
Tarek <tareksaad.dev@gmail.com>
2026-01-21 05:18:07 +0200

    new

--- src/app/page.js
+++ src/app/page.js
@@ -9,27 +9,32 @@ const Model = dynamic(() =>
         ssr: false,
     });
 
-export default function Home() {
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-center relative">
-            <Image 
-                priority 
-                sizes="100vw"
-                src={bg}
-                alt="background-image"
-                fill 
-                className="-z-50 w-full h-full object-cover object-center opacity-50"
-                quality={85}
-                placeholder="blur"
-                blurDataURL="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAIAAoDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAv/xAAhEAACAQMDBQAAAAAAAAAAAAABAgMABAUGIWGRkqGx0f/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAGhEAAgIDAAAAAAAAAAAAAAAAAAECEgMRkf/aAAwDAQACEQMRAD8AltJagyeH0AthI5xdrLcNM91BF5pX2HaH9bcfaSXWGaRmknyJckliyjqTzSlT54b6bk+h0R//2Q=="
-            />
+// new changes
 
-            <div className="w-full h-screen flex items-center justify-center">
-                <Navigation />
-                <RenderModel>
-                    <Model />
-                </RenderModel>
-            </div>
-        </main>
+export default function Home() {
+    return ( <
+        main className = "flex min-h-screen flex-col items-center justify-center relative" >
+        <
+        Image priority sizes = "100vw"
+        src = { bg }
+        alt = "background-image"
+        fill className = "-z-50 w-full h-full object-cover object-center opacity-50"
+        quality = { 85 }
+        placeholder = "blur"
+        blurDataURL = "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAIAAoDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAv/xAAhEAACAQMDBQAAAAAAAAAAAAABAgMABAUGIWGRkqGx0f/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAGhEAAgIDAAAAAAAAAAAAAAAAAAECEgMRkf/aAAwDAQACEQMRAD8AltJagyeH0AthI5xdrLcNM91BF5pX2HaH9bcfaSXWGaRmknyJckliyjqTzSlT54b6bk+h0R//2Q==" /
+        >
+
+        <
+        div className = "w-full h-screen flex items-center justify-center" >
+        <
+        Navigation / >
+        <
+        RenderModel >
+        <
+        Model / >
+        <
+        /RenderModel> <
+        /div> <
+        /main>
     );
 }
